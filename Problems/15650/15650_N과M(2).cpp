4f6c9862f02d254d45563794302d9079a56385a5
kyu9341 <kyu9341@naver.com>
2020-02-08 13:35:56 +0900

    15650

--- Problems/15650/15650_N과M(2).cpp
+++ Problems/15650/15650_N과M(2).cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 
 using namespace std;
-bool check[10];
 int a[10];
 void go(int index, int start, int n, int m)
 {
@@ -23,10 +22,32 @@ void go(int index, int start, int n, int m)
 	}				// 항상 더 큰 수가 뒤에 옴
 }
 
+void go2(int index, int selected, int n, int m) // go2 : index라는 수를 결과에 포함할건지 말지를 결정
+//  index : 실제 자연수 // selected : , 지금까지 선택한 수의 개수
+{		
+	if (selected == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << a[i];
+			if (i != m) cout << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	if (index > n) return; // index가 n보다 커지는 경우 리턴
+	a[selected] = index;		// index를 결과에 추가하는 경우
+	go2(index + 1, selected + 1, n, m);
+	a[selected] = 0;			// index를 결과에 추가하지 않는 경우
+	go2(index + 1, selected, n, m);
+}
+
+
 int main(void)
 {
 	int n, m;
 	cin >> n >> m;
 
-	go(0, 1, n, m);
+	go2(1, 0, n, m);
+	// go(0, 1, n, m);
 }
