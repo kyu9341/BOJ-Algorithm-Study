10a1ced1c61b9072aae566b9e4254e59a144f08f
kyu9341 <kyu9341@naver.com>
2020-02-14 15:32:11 +0900

    11724

---
+++ Problems/11724/11724연결요소의개수.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+vector<int> a[1001];
+bool check[1001];
+
+void dfs(int node)
+{
+	check[node] = true; // 방문 처리
+
+	for (int i = 0; i < a[node].size(); i++) // 현재 노드와 연결된 모든 노드 확인
+	{
+		int next = a[node][i]; // 다음 확인할 노드
+		if (!check[next]) // 다음 노드가 방문하지 않은 노드라면
+		{
+			dfs(next); // 다음 노드 탐색
+		}
+	}
+}
+
+int main(void)
+{
+	int n, m;
+	cin >> n >> m;
+
+	while (m--)
+	{
+		int u, v;
+		cin >> u >> v;
+		a[u].push_back(v);
+		a[v].push_back(u);
+	}
+	int cnt = 0; // 연결요소의 개수
+
+	for (int i = 1; i <= n; i++) // 모든 노드를 확인
+	{
+		if (!check[i]) // 방문하지 않은 노드이면 탐색
+		{		// 이미 방문했던 노드는 어떤 하나의 연결 요소에 포함되었을 것임
+			dfs(i);
+			cnt++;
+		}
+	}
+		
+	cout << cnt << '\n';
+}
