f26172ed573ab1652eac5f14e9fbe768a7fedc70
kyu9341 <kyu9341@naver.com>
2020-02-16 13:44:36 +0900

    Update 1707이분그래프.cpp

--- Problems/1707/1707이분그래프.cpp
+++ Problems/1707/1707이분그래프.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<queue>
 
 using namespace std;
 vector<int> a[20001];
@@ -20,6 +21,35 @@ bool dfs(int node, int c) // c : 다음 방문할 노드의 그룹 ( 1 or 2 ) , 이분그래프
 		}
 		else if (check[next] == check[node]) // 다음 정점이 이미 방문했던 정점인데 현재 정점과 같은 그룹이면 이분그래프가 아님
 			return false;
+	}
+	return true;
+}
+
+bool bfs(int start, int c)
+{
+	queue<int> q;
+	check[start] = c; // 그룹 지정
+	q.push(start); // 첫 노드 push
+
+	while (!q.empty())
+	{
+		int node = q.front();
+		q.pop(); 
+
+		for (int i = 0; i < a[node].size(); i++)
+		{
+			int next = a[node][i];
+			if (check[next] == 0)
+			{
+				c = 3 - check[node]; 
+				check[next] = c; // 다음 노드는 현재 그룹이 1이라면 2, 2라면 1로 방문 처리
+				q.push(next); // 방문처리하며 push
+			}
+			else if (check[node] == check[next])
+			{
+				return false;
+			}
+		}
 	}
 	return true;
 }
@@ -53,7 +83,7 @@ int main(void)
 		{
 			if (check[i] == 0) // 아직 방문 안한 노드에 대해서 확인
 			{
-				if (dfs(i, 1) == false)
+				if (bfs(i, 1) == false) // dfs(i, 1) 도 가능
 					ok = false;
 			}
 			
