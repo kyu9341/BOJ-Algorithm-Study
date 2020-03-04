3f9f53abcfe66823b63574d8e1954404a1ba7e75
kyu9341 <kyu9341@naver.com>
2020-03-04 13:25:34 +0900

    Create 11650좌표정렬하기.cpp

---
+++ Problems/11650/11650좌표정렬하기.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int main(void)
+{
+	int n;	
+	cin >> n;
+
+	vector<pair<int, int>> a(n);
+	for (int i = 0; i < n; i++)
+		cin >> a[i].first >> a[i].second;
+		
+	sort(a.begin(), a.end());
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i].first << ' ' << a[i].second;
+		cout << '\n';
+	}
+}
